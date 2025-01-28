d7b51413e884cb7943cde3b920fee0f1085d2641
Carlos Huaccho <Carlos@gmail.com>
2025-01-28 15:56:05 -0500

    Adding the models we will use for this project. We will use Artist, Album, and Song models. Artist should have a Many-to-Many relationship to Album and Song. Album and Song will have a Foreign-Key relationship. All of this data will be retrieved using Spotify API.

---
+++ spofify_integration/spofify_integration/models.py
@@ -0,0 +1,51 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+
+
+class Artist(models.Model):
+    """
+    Model for music artists.
+    """
+
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Album(models.Model):
+    """
+    Model for albums.
+    """
+
+    class Meta:
+        unique_together = ("name", "release_date")
+
+    name = models.CharField(max_length=255)
+    release_date = models.DateField()
+    artists = models.ManyToManyField(to=Artist, related_name="albums", blank=True)
+    type = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Song(models.Model):
+    """
+    Model for songs.
+    """
+
+    name = models.CharField(max_length=255)
+    artists = models.ManyToManyField(to=Artist, related_name="songs", blank=True)
+    album = models.ForeignKey(Album, related_name="songs", on_delete=models.CASCADE)
+    release_date = models.DateField()
+    popularity = models.IntegerField(
+        validators=[
+            MinValueValidator(limit_value=0),
+            MaxValueValidator(limit_value=100),
+        ],
+        help_text="Popularity score should be between 0 and 100.",
+    )
+
+    def __str__(self) -> str:
+        return f"{self.name} ({self.album.name if self.album else 'Single'})"
