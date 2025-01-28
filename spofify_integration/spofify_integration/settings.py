4f9a72f6f1b21e6e72eee44354910162139a2191
Carlos Huaccho <Carlos@gmail.com>
2025-01-28 15:49:40 -0500

    Adding templates directories and static files to settings

--- spofify_integration/spofify_integration/settings.py
+++ spofify_integration/spofify_integration/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "spofify_integration",  # Ensure this matches your app name
 ]
 
 MIDDLEWARE = [
@@ -57,7 +58,11 @@ ROOT_URLCONF = "spofify_integration.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [
+            os.path.join(
+                BASE_DIR, "spofify_integration", "templates", "spotify_templates"
+            )
+        ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -119,6 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
